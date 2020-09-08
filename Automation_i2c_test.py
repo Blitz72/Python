dc5ea799defdc31f76c4e2ef7918a325f3e74ebb
Blitz72 <bauer.david@att.net>
2020-09-08 13:38:57 -0400

    Update Automation_i2c_test.py
    Modified Automation_i2c_test.py.  Added docstrings, refactored some logic.

--- Automation_i2c_test.py
+++ Automation_i2c_test.py
@@ -1,110 +1,111 @@
 import smbus
 from time import sleep
 
-channel = 1
-address1 = 0x24
-address2 = 0x25
-IODIRA = 0x00
-IODIRB = 0x01
-GPIOA = 0x12
-GPIOB = 0x13
+CHANNEL = 1
+IO_ADDRESS1 = 0x24  # i2c address of first MCP23017
+IO_ADDRESS2 = 0x25  # i2c address of second MCP23017
+# MUX_ADDRESS1 = 0x70
+# MUX_ADDRESS2 = 0x71
+# MUX_ADDRESS3 = 0x72
+# SENSOR_ADDRESS = 0x29
+IO_IODIRA = 0x00    # address of I/O direction register for PORT A
+IO_IODIRB = 0x01    # address of I/O direction register for PORT B 
+IO_GPIOA = 0x12     # address of GPIO PORT A register 
+IO_GPIOB = 0x13     # address of GPIO PORT B register
 
-time_delay = 0.5
+# time_delay = 0.5  # used for cycle_outputs()
 
-try:
-   bus = smbus.SMBus(channel)
-except Exception as ex:
-    print(ex)
-    print("Failed to initialize i2c bus.")
 
-try:
-    bus.write_byte_data(address1, IODIRA, 0x00)
-    bus.write_byte_data(address1, IODIRB, 0x00)
-except Exception as ex:
-    print(ex)
-    print('An error occurred writing to:', hex(address1))
-try:
-    bus.write_byte_data(address2, IODIRA, 0x00)
-    bus.write_byte_data(address2, IODIRB, 0x00)
-except Exception as ex:
-    print(ex)
-    print('An error occurred writing to:', hex(address2))
+# assign configuration varaibles based on relay number
+def get_config(relay_num):
+    """Get the i2c address and corresponding GPIO register based on the relay number.
+
+    Inputs: relay_num, must be an integer 1 to 19 inclusive.
+    Returns: address, gpio_reg
+    """
+    if 1 <= relay_num <= 8:
+        address = 0x24
+        gpio_reg = IO_GPIOA
+    elif 9 <= relay_num <= 16:
+        address = 0x24
+        gpio_reg = IO_GPIOB
+    elif 17 <= relay_num <= 19:
+        address = 0x25
+        gpio_reg = IO_GPIOA
+    else:
+        raise ValueError('Please enter a value between 1 and 19')
+
+    return address, gpio_reg
 
 
 # turn off all relays
 def clear_outputs():
-    bus.write_byte_data(address1, GPIOA, 0x00)
-    bus.write_byte_data(address1, GPIOB, 0x00)
-    bus.write_byte_data(address2, GPIOA, 0x00)
+    """Turn off all relays."""
+    bus.write_byte_data(IO_ADDRESS1, IO_GPIOA, 0x00)
+    bus.write_byte_data(IO_ADDRESS1, IO_GPIOB, 0x00)
+    bus.write_byte_data(IO_ADDRESS2, IO_GPIOA, 0x00)
 
 
 # turn on all relays
 def all_outputs_on():
-    bus.write_byte_data(address1, GPIOA, 0xff)
-    bus.write_byte_data(address1, GPIOB, 0xff)
-    bus.write_byte_data(address2, GPIOA, 0x07)
+    """Turn on all relays."""
+    bus.write_byte_data(IO_ADDRESS1, IO_GPIOA, 0xff)
+    bus.write_byte_data(IO_ADDRESS1, IO_GPIOB, 0xff)
+    bus.write_byte_data(IO_ADDRESS2, IO_GPIOA, 0x07)
 
 
 # for early testing of MCP23017 boards, not to be used when testing individual 120VAC loads
-def cycle_outputs():
-    
-    for y in range(10):
-        for x in range(8):
-            bus.write_byte_data(address1, GPIOA, 1<<x)
-            sleep(time_delay)
-        bus.write_byte_data(address1, GPIOA, 0)
-        for x in range(8):
-            bus.write_byte_data(address1, GPIOB, 1<<x)
-            sleep(time_delay)
-        bus.write_byte_data(address1, GPIOB, 0)
-        for x in range(8):
-            bus.write_byte_data(address2, GPIOA, 1<<x)
-            sleep(time_delay)
-        bus.write_byte_data(address2, GPIOA, 0)
+# def cycle_outputs():
+#     
+#     for y in range(10):
+#         for x in range(8):
+#             bus.write_byte_data(IO_ADDRESS1, IO_GPIOA, 1<<x)
+#             sleep(time_delay)
+#         bus.write_byte_data(IO_ADDRESS1, IO_GPIOA, 0)
+#         for x in range(8):
+#             bus.write_byte_data(IO_ADDRESS1, IO_GPIOB, 1<<x)
+#             sleep(time_delay)
+#         bus.write_byte_data(IO_ADDRESS1, IO_GPIOB, 0)
+#         for x in range(8):
+#             bus.write_byte_data(IO_ADDRESS2, IO_GPIOA, 1<<x)
+#             sleep(time_delay)
+#         bus.write_byte_data(IO_ADDRESS2, IO_GPIOA, 0)
 
 
 # reset all devices in bay using V2 sequence
-def reset_all():
-    
-    clear_outputs()
-    sleep(5)
-    for x in range(5):
-        all_outputs_on()
-        sleep(8)
-        clear_outputs()
-        sleep(2)
-    all_outputs_on()
+# def reset_all():
+#     """Reset all devices in the bay using the C by GE V2 sequence."""
+#     clear_outputs()
+#     sleep(5)
+#     for x in range(5):
+#         all_outputs_on()
+#         sleep(8)
+#         clear_outputs()
+#         sleep(2)
+#     all_outputs_on()
     
 
-# b = bus.read_byte_data(address2, GPIOA)
+# b = bus.read_byte_data(IO_ADDRESS2, IO_GPIOA)
 # print(b)
 # print(~b & 0xff)
 
 
 # reset an individual relay
 def reset_relay(relay_num):
-    
-    # test to see if valid relay number, then assign proper address and GPIO register
-    if not 1 <= relay_num <= 19:
-        print('Please enter a relay number between 1 and 19')
-        return
-    if 1 <= relay_num <= 8:
-        address = 0x24
-        gpio_reg = GPIOA
-    elif 9 <= relay_num <= 16:
-        address = 0x24
-        gpio_reg = GPIOB
-    else:
-        address = 0x25
-        gpio_reg = GPIOA
+    """Reset individual relay using the C by GE V2 sequence.
+
+    Inputs: relay_num, must be an integer between 1 and 19 inclusive.
+    """
+    address, gpio_reg = get_config(relay_num)
         
-    # see what's already in ON/OFF in the register
+    # see if the relay is already ON/OFF in the GPIOx register
     b = bus.read_byte_data(address, gpio_reg)
-    print('GPIO register: ', b)
+#     print('i2c address:    ', hex(address))
+#     print('GPIO reg value: ', hex(b))
     
     # generate a bitmask for turning the relay off with bitwise &
     bitmask = ~(1 << ((relay_num - 1) % 8)) & 0xff
-    print('bitmask: ', bitmask)
+#     print('bitmask: ', bitmask)
     
     # turn the relay off for 5 seconds
     bus.write_byte_data(address, gpio_reg, b & bitmask)
@@ -119,30 +120,72 @@ def reset_relay(relay_num):
     bus.write_byte_data(address, gpio_reg, (1 << ((relay_num - 1) % 8)) | b)
 
 
-# toggle an individual relay by wire number
-def toggle_relay(relay_num):
-    
-    # test to see if valid relay number, then assign proper address and GPIO register
-    if not 1 <= relay_num <= 19:
-        print('Please enter a relay number between 1 and 19')
-        return
-    if 1 <= relay_num <= 8:
-        address = 0x24
-        gpio_reg = GPIOA
-    elif 9 <= relay_num <= 16:
-        address = 0x24
-        gpio_reg = GPIOB
-    else:
-        address = 0x25
-        gpio_reg = GPIOA
+def relay_on(relay_num):
+    """Turn on an individual relay.
+
+    Inputs: relay_num, must be an integer between 1 and 19 inclusive.
+    """
+    address, gpio_reg = get_config(relay_num)
+    print(hex(address), hex(gpio_reg))
+
+    # see what's already in ON/OFF in the register
+    b = bus.read_byte_data(address, gpio_reg)
+    print('i2c address:    ', hex(address))
+    print('GPIO reg vlaue: ', hex(b))
+
+    # generate a bitmask for turning the relay off with bitwise &
+    bitmask = ~(1 << ((relay_num - 1) % 8)) & 0xff
+    print('bitmask: ', bitmask)
+
+    # write new value to the proper GPIO address
+    bus.write_byte_data(address, gpio_reg, (1 << ((relay_num - 1) % 8)) | b)
+
+
+def relay_off(relay_num):
+    """Toggle an individual relay.
+
+    Inputs: relay_num, must be an integer between 1 and 19 inclusive.
+    """
+    address, gpio_reg = get_config(relay_num)
+    print(hex(address), hex(gpio_reg))
     
     # see what's already in ON/OFF in the register
     b = bus.read_byte_data(address, gpio_reg)
-    print('GPIO register: ', b)
+    print('i2c address:    ', hex(address))
+    print('GPIO reg vlaue: ', hex(b))
     
     # generate a bitmask for turning the relay off with bitwise &
     bitmask = ~(1 << ((relay_num - 1) % 8)) & 0xff
     print('bitmask: ', bitmask)
+
+    # write new value to the proper GPIO address
+    bus.write_byte_data(address, gpio_reg, b & bitmask)
+
+
+def power_cycle(relay_num):
+    """Power cycle a specific relay."""
+    relay_off(relay_num)
+    sleep(2)
+    relay_on(relay_num)
+
+
+# toggle an individual relay by wire number
+def toggle_relay(relay_num):
+    """Toggle an individual relay.
+
+    Inputs: relay_num, must be an integer between 1 and 19 inclusive.
+    """
+    address, gpio_reg = get_config(relay_num)
+#     print(hex(address), hex(gpio_reg))
+    
+    # see what's already in ON/OFF in the register
+    b = bus.read_byte_data(address, gpio_reg)
+#     print('i2c address:    ', hex(address))
+#     print('GPIO reg vlaue: ', hex(b))
+    
+    # generate a bitmask for turning the relay off with bitwise &
+    bitmask = ~(1 << ((relay_num - 1) % 8)) & 0xff
+#     print('bitmask: ', bitmask)
     
     # test to see if the relay is already on
     if (1 << ((relay_num - 1) % 8)) & b:
@@ -153,11 +196,36 @@ def toggle_relay(relay_num):
         bus.write_byte_data(address, gpio_reg, (1 << ((relay_num - 1) % 8)) | b)
 
 
+# initialize i2c bus
+try:
+   bus = smbus.SMBus(CHANNEL)
+except Exception as ex:
+    print(ex)
+    print("Failed to initialize i2c bus.")
+
+
+# POR (power on reset) values of IO_IODIRA and IO_IODIRB are 0xFF,
+# if bit(n) of IODIRX is 1 it is input, if 0 it is an output,
+# so must write 0x00 to configure the GPIO registers as outputs
+try:
+    bus.write_byte_data(IO_ADDRESS1, IO_IODIRA, 0x00)  # declare PORT A as outputs
+    bus.write_byte_data(IO_ADDRESS1, IO_IODIRB, 0x00)  # declare PORT B as outputs
+except Exception as ex:
+    print(ex)
+    print('An error occurred writing to:', hex(IO_ADDRESS1))
+try:
+    bus.write_byte_data(IO_ADDRESS2, IO_IODIRA, 0x00)  # declare PORT A as outputs
+    bus.write_byte_data(IO_ADDRESS2, IO_IODIRB, 0xff)  # declare PORT B as inputs
+except Exception as ex:
+    print(ex)
+    print('An error occurred writing to:', hex(IO_ADDRESS2))
+
+
 # toggle_relay(16)
-#all_outputs_on()
-#reset_relay(19)
-#all_outputs_on()
-#sleep(15)
-#reset_all()
+# all_outputs_on()
+# reset_relay(19)
+# all_outputs_on()
+# reset_all()
 # clear_outputs()
 # cycle_outputs()
+
