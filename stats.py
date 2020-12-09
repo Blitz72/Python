ead3063b0d4d35dcc575010a15e6797cae142f59
Blitz72 <bauer.david@att.net>
2020-12-09 08:10:09 -0500

    Modified stats.py to clean up cmd statement.

--- stats.py
+++ stats.py
@@ -39,7 +39,11 @@ i2c = busio.I2C(SCL, SDA)
 # Create the SSD1306 OLED class.
 # The first two parameters are the pixel width and pixel height.  Change these
 # to the right size for your display!
-disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+try:
+    disp = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c)
+except Exception as ex:
+    err = True
+    print(ex)
 
 # Clear display.
 disp.fill(0)
@@ -47,6 +51,7 @@ disp.show()
 
 # Create blank image for drawing.
 # Make sure to create image with mode '1' for 1-bit color.
+
 width = disp.width
 height = disp.height
 image = Image.new("1", (width, height))
@@ -65,12 +70,11 @@ bottom = height - padding
 # Move left to right keeping track of the current x position for drawing shapes.
 x = 0
 
-
 # Load default font.
 font = ImageFont.load_default()
 
 # Alternatively load a TTF font.  Make sure the .ttf font file is in the
-# same directory as the python script!
+    # same directory as the python script!
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
 # font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 9)
 
@@ -85,9 +89,12 @@ while True:
     IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
     #cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
     #CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "sudo iwgetid"
-    SSID = subprocess.check_output(cmd, shell=True).decode("utf-8").split(' ')[5]
-    SSID = SSID.split(':')[1]
+    #cmd = "sudo iwgetid"
+    #SSID = subprocess.check_output(cmd, shell=True).decode("utf-8").split(' ')[5]
+    #SSID = SSID.split(':')[1]
+    cmd = "sudo iwgetid | cut -d' ' -f6 | cut -d':' -f2"
+    SSID = subprocess.check_output(cmd, shell=True).decode("utf-8").strip('\"')
+    print(SSID)
     #cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
     #MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
     #cmd = 'df -h | awk \'$NF=="/"{printf "Disk: %d/%d GB  %s", $3,$2,$5}\''
@@ -104,4 +111,3 @@ while True:
     disp.image(image)
     disp.show()
     time.sleep(1.0)
-
