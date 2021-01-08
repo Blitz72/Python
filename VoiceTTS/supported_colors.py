0a21ac105b92bc4a3212511c6b26014c4953cba5
Blitz72 <bauer.david@att.net>
2021-01-08 16:20:49 -0500

    Modified supported_colors.py

--- VoiceTTS/supported_colors.py
+++ VoiceTTS/supported_colors.py
@@ -1,215 +1,215 @@
 supported_colors_list = [
     {
         'name': 'alice blue',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (240, 248, 255)
     },
-    {
-        'name': 'almond',
-        'rgb_values': (0, 0, 0)
-    },
-    {
-        'name': 'antique white',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'almond',
+#         'rgb_values': (0, 0, 0)
+#     },
+#     {
+#         'name': 'antique white',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'aqua',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (0, 255, 255)
     },
     {
         'name': 'aquamarine',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (127, 255, 212)
     },
     {
         'name': 'azure',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (0, 127, 255)
     },
-    {
-        'name': 'beige',
-        'rgb_values': (0, 0, 0)
-    },
-    {
-        'name': 'bisque',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'beige',
+#         'rgb_values': (0, 0, 0)
+#     },
+#     {
+#         'name': 'bisque',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'black',
         'rgb_values': (0, 0, 0)
     },
-    {
-        'name': 'blanched almond',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'blanched almond',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'blue',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (0, 0, 255)
     },
     {
         'name': 'blue violet',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (138, 43, 226)
     },
     {
         'name': 'brown',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (150, 75, 0)
     },
-    {
-        'name': 'burlywood',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'burlywood',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'cadet blue',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (95, 158, 160)
     },
     {
         'name': 'charteuse',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (223, 255, 0)
     },
     {
         'name': 'chocolate',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (123, 63, 0)
     },
     {
         'name': 'coral',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (255, 127, 80)
     },
     {
         'name': 'cornflower',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (100, 149, 237)
     },
-    {
-        'name': 'cornsilk',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'cornsilk',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'crimson',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (220, 20, 60)
     },
     {
         'name': 'cyan',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (0, 255, 255)
     },
     {
         'name': 'dark blue',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (0, 0, 139)
     },
     {
         'name': 'dark cyan',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (0, 139, 139)
     },
     {
         'name': 'dark goldenrod',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (184, 134, 11)
     },
     {
         'name': 'dark gray',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (169, 169, 169)
     },
     {
         'name': 'dark green',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (1, 50, 32)
     },
     {
         'name': 'dark khaki',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (189, 183, 107)
     },
     {
         'name': 'dark magenta',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (139, 0, 139)
     },
-    {
-        'name': 'dark olive green',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'dark olive green',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'dark orange',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (85, 107, 47)
     },
     {
         'name': 'dark orchid',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (153, 50, 204)
     },
     {
         'name': 'dark red',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (139, 0, 0)
     },
     {
         'name': 'dark salmon',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (233, 150, 122)
     },
-    {
-        'name': 'dark sea green',
-        'rgb_values': (0, 0, 0)
-    },
-    {
-        'name': 'dark slate blue',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'dark sea green',
+#         'rgb_values': (0, 0, 0)
+#     },
+#     {
+#         'name': 'dark slate blue',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'dark slate gray',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (47, 79, 79)
     },
     {
         'name': 'dark turquoise',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (0, 206, 209)
     },
     {
         'name': 'dark violet',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (148, 0, 211)
     },
     {
         'name': 'deep pink',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (255, 20, 147)
     },
     {
         'name': 'deep sky blue',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (0, 191, 255)
     },
     {
         'name': 'dim gray',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (105, 105, 105)
     },
     {
         'name': 'dodger blue',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (30, 144, 255)
     },
     {
         'name': 'firebrick',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (178, 34, 34)
     },
-    {
-        'name': 'floral white',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'floral white',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'forest green',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (34, 139, 34)
     },
     {
         'name': 'fuchsia',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (193, 84, 193)
     },
-    {
-        'name': 'gainsboro',
-        'rgb_values': (0, 0, 0)
-    },
-    {
-        'name': 'garnet',
-        'rgb_values': (0, 0, 0)
-    },
-    {
-        'name': 'ghost white',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'gainsboro',
+#         'rgb_values': (0, 0, 0)
+#     },
+#     {
+#         'name': 'garnet',
+#         'rgb_values': (0, 0, 0)
+#     },
+#     {
+#         'name': 'ghost white',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'gold',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (220, 220, 220)
     },
     {
         'name': 'goldenrod',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (218, 165, 32)
     },
     {
         'name': 'gray',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (128, 128, 128)
     },
     {
         'name': 'green',
@@ -217,23 +217,23 @@ supported_colors_list = [
     },
     {
         'name': 'green yellow',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (173, 255, 47)
     },
     {
         'name': 'honeydew',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (240, 255, 240)
     },
     {
         'name': 'hot pink',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (255, 105, 180)
     },
     {
         'name': 'indian red',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (205, 92, 92)
     },
     {
         'name': 'indigo',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (75, 0, 130)
     },
     {
         'name': 'ivory',
@@ -241,162 +241,163 @@ supported_colors_list = [
     },
     {
         'name': 'khaki',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (195, 176, 145)
     },
     {
         'name': 'lavender',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (181, 126, 220)
     },
     {
         'name': 'lavender blush',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (255, 240, 245)
     },
     {
         'name': 'lawn green',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (124, 252, 0)
     },
     {
         'name': 'lemon chiffon',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (255, 250, 205)
     },
     {
         'name': 'light blue',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (173, 216, 230)
     },
     {
-        'name': 'ligth coral',
-        'rgb_values': (0, 0, 0)
+        'name': 'ligth coral',         # The RGB values shown here come from "hey google turn the lights to the color light coral.'
+        'rgb_values': (240, 128, 128)  # Google results vary using 'light coral' and 'the color light coral'
     },
     {
-        'name': 'light cyan',
-        'rgb_values': (0, 0, 0)
+        'name': 'light cyan',          # The RGB values shown here come from "hey google turn the lights to the color light cyan.'
+        'rgb_values': (224, 255, 255)  # Google results vary using 'light cyan' and 'the color light cyan'
+    },
+#     {
+#         'name': 'light goldenrod',
+#         'rgb_values': (0, 0, 0)
+#     },
+    {
+        'name': 'light gray',          # The RGB values shown here come from "hey google turn the lights to the color light gray.'
+        'rgb_values': (211, 211, 211)  # Google results vary using 'light gray' and 'the color light gray'
     },
     {
-        'name': 'light goldenrod',
-        'rgb_values': (0, 0, 0)
+        'name': 'light green',         # The RGB values shown here come from "hey google turn the lights to the color light green.'
+        'rgb_values': (144, 238, 144)  # Google results vary using 'light green' and 'the color light green'
     },
     {
-        'name': 'light gray',
-        'rgb_values': (0, 0, 0)
+        'name': 'light pink',          # The RGB values shown here come from "hey google turn the lights to the color light pink.'
+        'rgb_values': (255, 182, 193)  # Google results vary using 'light pink' and 'the color light pink'
     },
     {
-        'name': 'light green',
-        'rgb_values': (0, 0, 0)
-    },{
-        'name': 'light pink',
-        'rgb_values': (0, 0, 0)
+        'name': 'light salmon',        # The RGB values shown here come from "hey google turn the lights to the color light salmon.'
+        'rgb_values': (255, 160, 122)  # Google results vary using 'light pink' and 'the color light salmon'
     },
     {
-        'name': 'light salmon',
-        'rgb_values': (0, 0, 0)
+        'name': 'light sea green',     # The RGB values shown here come from "hey google turn the lights to the color light salmon.'
+        'rgb_values': (32, 178, 170)   # Google results vary using 'light sea green' and 'the color light sea green'
     },
     {
-        'name': 'light sea green',
-        'rgb_values': (0, 0, 0)
+        'name': 'light sky blue',      # The RGB values shown here come from "hey google turn the lights to the color light sky blue.'
+        'rgb_values': (135, 206, 250)  # Google results vary using 'light sea sky blue' and 'the color light sea ky blue'
     },
     {
-        'name': 'light sky blue',
-        'rgb_values': (0, 0, 0)
+        'name': 'light slate gray',    # The RGB values shown here come from "hey google turn the lights to the color light slate gray.'
+        'rgb_values': (119, 136, 153)  # Google results vary using 'light sea green' and 'the color light slate gray'
     },
     {
-        'name': 'light slate gray',
-        'rgb_values': (0, 0, 0)
+        'name': 'light steel blue',    # The RGB values shown here come from "hey google turn the lights to the color light steel blue.'
+        'rgb_values': (176, 196, 222)  # Google results vary using 'light sea green' and 'the color light steel blue'
     },
     {
-        'name': 'light steel blue',
-        'rgb_values': (0, 0, 0)
-    },
-    {
-        'name': 'light yellow',
-        'rgb_values': (0, 0, 0)
+        'name': 'light yellow',        # The RGB values shown here come from "hey google turn the lights to the color light yellow.'
+        'rgb_values': (255, 255, 224)  # Google results vary using 'light sea green' and 'the color light yellow'
     },
     {
         'name': 'lime',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (191, 255, 0)
     },
     {
         'name': 'lime green',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (50, 205, 50)
     },
-    {
-        'name': 'linen',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'linen',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'magenta',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (255, 0, 255)
     },
     {
         'name': 'maroon',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (128, 0, 0)
     },
     {
         'name': 'medium aquamarine',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (102, 221, 170)
     },
     {
         'name': 'medium blue',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (0, 0, 205)
     },
-    {
-        'name': 'medium orchid',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'medium orchid',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'medium purple',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (147, 112, 219)
     },
     {
         'name': 'medium sea green',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (60, 179, 113)
     },
-    {
-        'name': 'medium slate blue',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'medium slate blue',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'medium spring green',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (0, 250, 154)
     },
     {
         'name': 'medium turquoise',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (72, 209, 204)
     },
-    {
-        'name': 'medium violet red',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'medium violet red',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'midnight blue',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (25, 25, 112)
     },
     {
         'name': 'mint cream',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (245, 255, 250)
     },
     {
         'name': 'misty rose',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (255, 228, 225)
     },
-    {
-        'name': 'moccasin',
-        'rgb_values': (0, 0, 0)
-    },
-    {
-        'name': 'navajo white',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'moccasin',
+#         'rgb_values': (0, 0, 0)
+#     },
+#     {
+#         'name': 'navajo white',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'navy blue',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (0, 0, 128)
     },
-    {
-        'name': 'old lace',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'old lace',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'olive',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (128, 128, 0)
     },
     {
         'name': 'olive drab',
@@ -404,43 +405,43 @@ supported_colors_list = [
     },
     {
         'name': 'olive green',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (107, 142, 35)
     },
     {
         'name': 'orange',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (255, 102, 0)
     },
     {
         'name': 'orange red',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (255, 75, 0)
     },
     {
         'name': 'orchid',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (218, 112, 214)
     },
-    {
-        'name': 'pale goldenrod',
-        'rgb_values': (0, 0, 0)
-    },
-    {
-        'name': 'pale green',
-        'rgb_values': (0, 0, 0)
-    },
-    {
-        'name': 'pale turquoise',
-        'rgb_values': (0, 0, 0)
-    },
-    {
-        'name': 'pale violet red',
-        'rgb_values': (0, 0, 0)
-    },
+#     {
+#         'name': 'pale goldenrod',
+#         'rgb_values': (0, 0, 0)
+#     },
+#     {
+#         'name': 'pale green',
+#         'rgb_values': (0, 0, 0)
+#     },
+#     {
+#         'name': 'pale turquoise',
+#         'rgb_values': (0, 0, 0)
+#     },
+#     {
+#         'name': 'pale violet red',
+#         'rgb_values': (0, 0, 0)
+#     },
     {
         'name': 'papaya whip',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (255, 239, 213)
     },
     {
         'name': 'peach puff',
-        'rgb_values': (0, 0, 0)
+        'rgb_values': (255, 218, 185)
     },
     {
         'name': 'peru',
