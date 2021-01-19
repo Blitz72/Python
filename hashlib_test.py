849e486582cf858f92f11c7bd04a18a8fbbdd856
Blitz72 <bauer.david@att.net>
2021-01-19 16:15:43 -0500

    Modified hashlib_test.py

--- hashlib_test.py
+++ hashlib_test.py
@@ -1,10 +1,17 @@
 import hashlib
+from hmac import compare_digest
 
 m = hashlib.sha256()
-m.update(b'Davey smells, really bad, dude!')
+m.update(b'Davey smells,')
+m.update(b' really bad, dude!')  # same as m.update(b'Davey smells, really bad, dude!')
 
+print(m.digest())
 print(m.hexdigest())
 print(m.digest_size)
 print(m.block_size)
 
 print(m.name)
+print(hashlib.algorithms_available)
+
+# compare = hmac.compare_digest(m, 'c0ab6671a6fc347b5fe99e542452f8ea1653340cc2449225295dbbb12b3cffc2')
+# print(compare)
