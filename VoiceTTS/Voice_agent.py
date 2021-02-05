542e3eb1c7211479101ffb024333145d8946a4cd
Blitz72 <bauer.david@att.net>
2021-02-05 15:58:44 -0500

    Modified VoiceTTS/Voice_agent.py and VoiceTTS/voice_test.py

--- VoiceTTS/Voice_agent.py
+++ VoiceTTS/Voice_agent.py
@@ -35,6 +35,7 @@ class Voice_agent:
     def speak(self, message):
         _filename = self.create_filename(message)
         # print(_filename)
+        # file_exists = False
         if not os.path.exists(self.filepath + f'{_filename}.mp3'):
             synthesis_input = texttospeech.SynthesisInput(text=message)
             voice = texttospeech.VoiceSelectionParams(
@@ -45,21 +46,40 @@ class Voice_agent:
             audio_config = texttospeech.AudioConfig(
                 audio_encoding=texttospeech.AudioEncoding.MP3
             )
-            _response = self.client.synthesize_speech(
-                input=synthesis_input, voice=voice, audio_config=audio_config
-            )
+            error = None
+            _response = None
+            try:
+                _response = self.client.synthesize_speech(
+                    input=synthesis_input, voice=voice, audio_config=audio_config
+                )
+            except Exception as ex:
+                error = ex
+            finally:
+                if error:
+                    print('google-cloud-texttospeech exception:', error)
+                elif _response:
+                    print('Audio content created!')
             with open(f"{self.filepath}{_filename}.mp3", "wb") as out:
                 # Write the response to the output file.
                 out.write(_response.audio_content)
                 print(f'Audio content written to file "{self.filepath}{_filename}.mp3"')
+                # file_exists = True
         else:
             print(f'{_filename}.mp3 already exists, file not created.')
+            # file_exists = True
+        error = None
+        process = None
         try:
+            # if file_exists:
             process = subprocess.check_output(f'omxplayer -o local {self.filepath}{_filename}.mp3', shell=True).decode('utf-8')
-            if 'have' in process:
-                print('File finished playing successfully!')
         except Exception as ex:
-            print('Subprocess exception:', ex)
+            error = ex
+        finally:
+            if error:
+                print('Subprocess exception:', error)
+            elif process:
+                if 'have' in process:
+                    print('File finished playing successfully!')
 
 
 # path = '/home/pi/Python/VoiceTTS/voice_files/'
--- VoiceTTS/voice_test.py
+++ VoiceTTS/voice_test.py
@@ -7,4 +7,4 @@ alexa = va.Voice_agent('alexa', path)
 # print(alexa.wake_word)
 # for color in alexa.cct_color_list:
 #     print(color['name'])
-alexa.speak(f'{alexa.wake_word}, turn the color bulb to medium spring green, please.')
+alexa.speak(f'{alexa.wake_word}, turn the color bulb to deep pink, please.')
