899a8f04bd9e5b06d91d4661669f82af0d7f68d8
Blitz72 <bauer.david@att.net>
2021-02-10 08:15:13 -0500

    Modified VoiceTTS/voice_test.py and added tca9548a_test.py

--- VoiceTTS/voice_test.py
+++ VoiceTTS/voice_test.py
@@ -9,8 +9,9 @@ from time import sleep
 # | wake word | launch |      invocation name       | utterance (the intent of the command)
 
 def set_power(group, values):
+    emphasis_level = 'moderate'
     for value in values:
-        print(va.speak(f'turn {value} the {group}, please'))
+        print(va.speak(f'turn <emphasis level="{emphasis_level}"> {value} </emphasis> the {group}, please'))
         sleep(15)
 
 def set_brightness(group, values):
@@ -18,20 +19,23 @@ def set_brightness(group, values):
         if value == 'dim' or value == 'brighten':
             added_str = ''
             launch_word = value
+            emphasis_level = 'moderate'
         else:
             added_str = f'to {value}'
             launch_word = 'set'
-        print(va.speak(f'{launch_word} the {group} {added_str}, please'))
+            emphasis_level = 'none'
+        print(va.speak(f'<emphasis level="{emphasis_level}"> {launch_word} </emphasis> the {group} {added_str}, please'))
         sleep(15)
 
 def set_color(group, values):
+    emphasis_level = "moderate"
     for value in values:
         # print(value)
         # if 'light' or 'tan' in value:
         #     added_str = 'the color '
         # else:
         #     added_str = ''
-        print(va.speak(f'turn the {group} to the color <emphasis> {value} </emphasis>, please'))
+        print(va.speak(f'turn the {group} to the color <emphasis level="{emphasis_level}"> {value} </emphasis>, please'))
         sleep(15)
 
 def make_warm_cool(group, values):
@@ -39,7 +43,7 @@ def make_warm_cool(group, values):
         va.speak(f'make {group} {value}, please')
         sleep(15)
 
-voice_agent = 'alexa'  # 'alexa' or 'google'
+voice_agent = 'google'  # 'alexa' or 'google'
 dir_name = voice_agent.capitalize()
 # print(dir_name)
 
@@ -60,7 +64,7 @@ group_name = 'office lights'
 
 # set_brightness(group_name, ['0%', 'brighten', 'brighten', 'brighten', 'brighten', 'brighten', 'brighten'])
 # set_brightness(group_name, ['9%', 'brighten', 'brighten', 'brighten', 'brighten', 'brighten', 'brighten'])
-# set_brightness(group_name, ['100%', 'dim', 'dim', 'dim', 'dim', 'dim', 'dim'])
+set_brightness(group_name, ['100%', 'dim', 'dim', 'dim', 'dim', 'dim', 'dim'])
 # set_brightness(group_name, ['92%', 'dim', 'dim', 'dim', 'dim', 'dim', 'dim'])
 
 # set_brightness(group_name, ['100%'])
@@ -73,10 +77,10 @@ group_name = 'office lights'
 # set_color(group_name, ['cool white'])
 # make_warm_cool(group_name, ['warmer', 'warmer', 'warmer', 'warmer', 'warmer', 'warmer'])
 
-color_list = []
-color_names = []
-for x in range(5):
-    color_list.append(va.rgb_color_list[random.randint(0, len(va.rgb_color_list) - 1)])
-for color in color_list:
-    color_names.append(color['name'])
-set_color(group_name, color_names)
+# color_list = []
+# color_names = []
+# for x in range(5):
+#     color_list.append(va.rgb_color_list[random.randint(0, len(va.rgb_color_list) - 1)])
+# for color in color_list:
+#     color_names.append(color['name'])
+# set_color(group_name, color_names)
