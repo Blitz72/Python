e9995cb9a36a2237bcf26dd4eb937fe3d8fcf76f
David Bauer <david.bauer@savant.com>
2021-06-14 13:03:47 -0400

    Modified test.py.

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import time
+import requests
 
 counter = 0
 
 for counter in range(2, 25, 2):
-  print("Hello world!: " + str(counter))
-  # time.sleep(0.1)
+  # print("Hello world!: " + str(counter))
+  print("Hello world!: ", counter)
+  time.sleep(0.2)
 
 
-message = input("Please enter a string: ") # \n is stripped from input
+# message = input("Please enter a string: ") # \n is stripped from input
 
-if len(message) < 6:
-  print("Your string is too short!")
+# if len(message) < 6:
+#   print("Your string is too short!")
+
+print(requests.get('https://www.google.com'))
