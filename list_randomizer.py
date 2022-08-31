385c0fa09a8448422fa210ddc841b9d15a149bda
DavidBauer3838 <david.bauer@savant.com>
2022-08-31 09:52:21 -0400

    Added list_randomizer.py.

---
+++ list_randomizer.py
@@ -0,0 +1,12 @@
+import random
+
+seeds = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+randomized_list = []
+
+length = len(seeds)
+for x in range(length):
+    chosen = seeds[random.randint(0, len(seeds)-1)]
+    seeds.remove(chosen)
+    randomized_list.append(chosen)
+    print(seeds)
+    print(randomized_list)
