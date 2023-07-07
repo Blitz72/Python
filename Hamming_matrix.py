31185bca8fecbcee378bf81456765f8f0cae2447
Blitz72 <bauer.david@att.net>
2023-07-07 10:58:03 -0400

    Update Hamming_matrix.py to add Hamming(4, 7) and Hamming(11, 15) matrices.

--- Hamming_matrix.py
+++ Hamming_matrix.py
@@ -35,6 +35,16 @@ hamming_7_4_H = [
     [0, 0, 0, 1, 1, 1, 1]
 ]
 
+hamming_4_7_H = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 0, 1],
+    [1, 0, 1],
+    [0, 1, 1],
+    [1, 1, 1]
+]
+
 # Hamming(15, 11) Generator Matrix
 hamming_15_11_G = [
     [1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
@@ -54,6 +64,20 @@ hamming_15_11_G = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 ]
 
+hamming_11_15_G = [
+    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+    [1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+    [1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+    [1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1]
+]
+
 # Hamming(15, 11) Parity Check Matrix
 hamming_15_11_H = [
     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
@@ -62,25 +86,51 @@ hamming_15_11_H = [
     [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-data_4 = [
+hamming_11_15_H = [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [1, 1, 0, 0],
+    [0, 0, 1, 0],
+    [1, 0, 1, 0],
+    [0, 1, 1, 0],
+    [1, 1, 1, 0],
+    [0, 0, 0, 1],
+    [1, 0, 0, 1],
+    [0, 1, 0, 1],
+    [1, 1, 0, 1],
+    [0, 0, 1, 1],
+    [1, 0, 1, 1],
+    [0, 1, 1, 1],
+    [1, 1, 1, 1]
+]
+
+data_7_4 = [
     [1],
     [0],
+    [0],
+    [0]
+]
+
+data_4_7 = [
+    [0, 1, 1, 0]
+]
+
+data_15_11 = [
+    [1],
+    [0],
+    [1],
+    [1],
+    [0],
+    [0],
+    [1],
+    [0],
+    [1],
     [1],
     [1]
 ]
 
-data_11 = [
-    [1],
-    [0],
-    [1],
-    [1],
-    [0],
-    [0],
-    [1],
-    [0],
-    [1],
-    [1],
-    [1]
+data_11_15 = [
+    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
 def matrix_mult(matrix_a, matrix_b):
@@ -115,11 +165,11 @@ def print_matrix(matrix):
 
 
 if __name__ == '__main__':
-    hamming_result_7_4 = matrix_mult(hamming_7_4_G, data_4)
+    hamming_result_7_4 = matrix_mult(hamming_7_4_G, data_7_4)
     for result in hamming_result_7_4:
         result[0] %= 2
 
-    print(f"Hamming(7, 4) result of {data_4[0][0]}{data_4[1][0]}{data_4[2][0]}{data_4[3][0]}:")
+    print(f"Hamming(7, 4) result of {data_7_4[0][0]}{data_7_4[1][0]}{data_7_4[2][0]}{data_7_4[3][0]}:")
     print_matrix(hamming_result_7_4)
 
     hamming_parity_check = matrix_mult(hamming_7_4_H, hamming_result_7_4)
@@ -129,14 +179,14 @@ if __name__ == '__main__':
     print("Hamming parity check of previous result:")
     print_matrix(hamming_parity_check)
 
-    hamming_result_15_11 = matrix_mult(hamming_15_11_G, data_11)
+    hamming_result_15_11 = matrix_mult(hamming_15_11_G, data_15_11)
     for result in hamming_result_15_11:
         result[0] %= 2
 
     print("Hamming(15, 11) result of ", end='')
-    for i in range (len(data_11) - 1):
-        print(f"{data_11[i][0]}", end='')
-    print(f"{data_11[len(data_11) - 1][0]}:")
+    for i in range (len(data_15_11) - 1):
+        print(f"{data_15_11[i][0]}", end='')
+    print(f"{data_15_11[len(data_15_11) - 1][0]}:")
     print_matrix(hamming_result_15_11)
 
     # Flip a random bit in the data_11 sequence and print the matrix for comparison
@@ -161,3 +211,30 @@ if __name__ == '__main__':
     for i in range(len(hamming_parity_check)):
         index += pow(2, i) * hamming_parity_check[i][0]
     print(f"The bit in position {index} was flipped!!!")
+    print('-----------------------------\n')
+
+    hamming_result_4_7 = matrix_mult(data_4_7, hamming_4_7_G)
+    for result in hamming_result_4_7[0]:
+        result %= 2
+    for i in range(len(hamming_result_4_7[0])):
+        hamming_result_4_7[0][i] %= 2
+    print_matrix(data_4_7)
+    print_matrix(hamming_result_4_7)
+
+    hamming_parity_check_4_7 = matrix_mult(hamming_result_4_7, hamming_4_7_H)
+    for i in range(len(hamming_parity_check_4_7[0])):
+        hamming_parity_check_4_7[0][i] %= 2
+    print_matrix(hamming_parity_check_4_7)
+
+    hamming_result_11_15 = matrix_mult(data_11_15, hamming_11_15_G)
+    for result in hamming_result_11_15[0]:
+        result %= 2
+    for i in range(len(hamming_result_11_15[0])):
+        hamming_result_11_15[0][i] %= 2
+    print_matrix(data_11_15)
+    print_matrix(hamming_result_11_15)
+
+    hamming_parity_check_11_15 = matrix_mult(hamming_result_11_15, hamming_11_15_H)
+    for i in range(len(hamming_parity_check_11_15[0])):
+        hamming_parity_check_11_15[0][i] %= 2
+    print_matrix(hamming_parity_check_11_15)
