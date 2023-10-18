8b857740599494105ad1feea85c69342293f5e5b
Blitz72 <bauer.david@att.net>
2023-10-18 09:42:19 -0400

    Create unicode.py

---
+++ unicode.py
@@ -0,0 +1,9 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--char", help="Print out 100 unicode characters starting at a base character num(decimal)! ex: -c 2600")
+args = parser.parse_args()
+
+char_num = int(args.char) if args.char else 0
+mystring = '  '.join(f"{chr(char_num + i)} {char_num + i}" for i in range(100))
+print(mystring)
