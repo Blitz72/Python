b6e435e8d468910f30dbfa28127583fc99b709ee
Blitz72 <bauer.david@att.net>
2023-10-19 10:51:40 -0400

    Update test.py

--- Udemy Python Data Structures/test.py
+++ Udemy Python Data Structures/test.py
@@ -1,26 +1,4 @@
-# def my_sum(*a):
-#     sum = 0
-#     for i in a:
-#         sum = sum + i
-#     print(sum)
-#
-# def display():
-#     print('Hello')
-#     return 0
-#
-# print(type(display()))
-# my_sum(1, 2, 3, display(), 4, 5, 6)
-
-# import math
-# print(dir(math))
-# math_contents = dir(math)
-# for item in math_contents:
-#     print(item)
-
-# from math import pi, e
-# print(pi)
-# print(e)
-# print(math.sqrt(25))
+#!/usr/bin/python3
 
 class Student:
     """This is class Student v1.0.0"""
@@ -45,20 +23,22 @@ class Student:
         print('Class Number:', self.roll)
         print('Grades:', self.__marks)
 
-S = Student('John', 101)
-S2 = Student('Dave', 102)
 
-print(S.college)
-print(S.name)
-print(S.roll)
-# print(S.__marks)    # Generates error
-print(S.__doc__)
-print(S)
-print(dir(S))
-S.display()
-S.__marks = 100.00
-print('New grades:', S.__marks)
-for student in Student.students:
-    print(student.name)
-print(Student.students)
-print(Student.count)
+if __name__ = '__main__':
+    S = Student('John', 101)
+    S2 = Student('Dave', 102)
+    
+    print(S.college)
+    print(S.name)
+    print(S.roll)
+    # print(S.__marks)    # Generates error
+    print(S.__doc__)
+    print(S)
+    print(dir(S))
+    S.display()
+    S.__marks = 100.00
+    print('New grades:', S.__marks)
+    for student in Student.students:
+        print(student.name)
+    print(Student.students)
+    print(Student.count)
