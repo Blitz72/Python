e49661f9811352fcdcf9d190e55425bbb01fc135
Blitz72 <bauer.david@att.net>
2023-10-19 08:07:55 -0400

    Update Hamming_matrix.py

--- Hamming_matrix.py
+++ Hamming_matrix.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 # This Python program is inspired from implementing the logic behind matrix
 # multiplication as found on the Wikipedia site:
 # https://en.wikipedia.org/wiki/Matrix_multiplication#
