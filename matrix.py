dab93990d9d28632802ed61ea93b545592248a2a
Blitz72 <bauer.david@att.net>
2023-12-08 08:31:20 -0500

    Update matrix.py
    Cleaned up the print_matrix function to better handle floats and only calculate the largest_in_col one time.

--- matrix.py
+++ matrix.py
@@ -8,7 +8,36 @@ https://en.wikipedia.org/wiki/Matrix_multiplication#
 
 from math import floor, log10
 
-def matrix_mult(_matrix_a, _matrix_b) -> list:
+def largest_in_col(_matrix, _col, _places):
+    neg = False
+    largest = 0
+    for row in _matrix:
+        if abs(row[_col]) > largest:
+            largest = abs(row[_col])
+        if round(row[_col], _places) < 0:
+            neg = True
+    return largest, neg
+
+def print_matrix(_matrix, _places=0):
+    largest_dict = {}
+    for col in range(len(_matrix[0])):
+        largest_dict[col] = {}
+        largest_dict[col]['largest'], largest_dict[col]['neg'] = largest_in_col(_matrix, col, _places)
+    for row in _matrix:
+        print('|', end=' ')
+        for col in range(len(row)):
+            largest = largest_dict[col]['largest']
+            neg = largest_dict[col]['neg']
+            largest = round(largest, _places) if round(largest, _places) != 0.0 else 1
+            row[col] = round(row[col], _places) if round(row[col], _places) != 0.0 else abs(round(row[col], _places))
+            value = round(row[col], _places) if round(row[col], _places) != 0.0 else 1    # log10(num), where num is not zero
+            end = ' ' * (floor(log10(abs(largest))) - floor(log10(abs(value))) + 1) if isinstance(value, int) else ' '
+            fmt_str = '{: .' +str(_places) + 'f}' if neg else '{:.' +str(_places) + 'f}'
+            num = fmt_str.format(row[col])
+            print(num, end=end)
+        print('|')
+
+def mult_matrix(_matrix_a, _matrix_b) -> list:
     matrix_c = [ [0] * len(_matrix_b[0]) for _ in range(len(_matrix_a))]
     # print_matrix(matrix_c)
     for y in range(len(_matrix_a)):
@@ -16,34 +45,6 @@ def matrix_mult(_matrix_a, _matrix_b) -> list:
             for z in range(len(_matrix_a[0])):
                 matrix_c[y][x] += _matrix_a[y][z] * _matrix_b[z][x]
     return matrix_c
-
-def largest_in_col(_matrix, _col):
-    neg = False
-    largest = 0
-    for row in _matrix:
-        if abs(row[_col]) > largest:
-            largest = abs(row[_col])
-        if row[_col] < 0:
-            neg = True
-    return largest, neg
-
-def print_matrix(_matrix, places=0):
-    neg = False
-    for row in _matrix:
-        print('|', end=' ')
-        for x in range(len(row)):
-            largest, neg = largest_in_col(_matrix, x)
-            if neg:
-                fmt_str = '{: .' +str(places) + 'f}'
-            else:
-                fmt_str = '{:.' +str(places) + 'f}'
-            largest = round(largest, places) if round(largest, places) != 0.0 else 1
-            row[x] = round(row[x], places) if round(row[x], places) != 0.0 else abs(round(row[x], places))
-            value = round(row[x], places) if round(row[x], places) != 0.0 else 1    # log10(num), where num is not zero
-            end = ' ' * (floor(log10(abs(largest))) - floor(log10(abs(value))) + 1)
-            num = fmt_str.format(row[x])
-            print(num, end=end)
-        print('|')
 
 def transpose_matrix(_matrix) -> list:
     matrix_transposed = [[0 for _ in range(len(_matrix))] for _ in range(len(_matrix[0]))]
@@ -65,10 +66,13 @@ if __name__ == "__main__":
         [4, 2, 2]
     ]
     print_matrix(matrix_A)
+    print()
     print_matrix(matrix_B)
-    
-    matrix_C = matrix_mult(matrix_A, matrix_B)
+    print()
+
+    matrix_C = mult_matrix(matrix_A, matrix_B)
     print_matrix(matrix_C)
+    print()
     
     matrix_D = [
         [100],
@@ -76,10 +80,11 @@ if __name__ == "__main__":
         [60]
     ]
     print_matrix(matrix_D)
+    print()
     
-    matrix_E = matrix_mult(matrix_C, matrix_D)
+    matrix_E = mult_matrix(matrix_C, matrix_D)
     print_matrix(matrix_E)
-
+    print()
 
     matrix_F = [
         [4, 7],
@@ -91,10 +96,26 @@ if __name__ == "__main__":
         [-0.2, 0.4]
     ]
 
-    matrix_H = matrix_mult(matrix_F, matrix_G)
-    # print(matrix_H)
-    print_matrix(matrix_H)
+    matrix_H = [
+        [1.23, 4.56],
+        [7.89, 10.11]
+    ]
+
+    matrix_I = mult_matrix(matrix_F, matrix_G)
+    # print(matrix_I)
+    print_matrix(matrix_I, _places=20)
+    print()
+
+    matrix_J = mult_matrix(matrix_G, matrix_H)
+    # print(matrix_J)
+    print_matrix(matrix_J, _places=4)
+    print()
+
+    print_matrix(transpose_matrix(matrix_J), _places=4)
+    print()
 
     matrix_A_trsnsposed = transpose_matrix(matrix_A)
     print_matrix(matrix_A)
+    print()
     print_matrix(matrix_A_trsnsposed)
+    print()
